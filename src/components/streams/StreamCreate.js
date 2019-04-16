7929e1c85cf09e3c1fe6f676a3f04f262b07f15d
Shehriyar Nadeem <shehriyarnadeem@Shehriyars-MacBook-Pro.local>
2019-04-16 14:26:51 +0500

    Section 20

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,7 +1,61 @@
 import React from 'react';
+import {Field, reduxForm} from 'redux-form';
+class StreamCreate extends React.Component{
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>;
-};
+  renderError=({error,touched})=>{
 
-export default StreamCreate;
+    if(touched && error){
+    return (
+    <div className="ui error message">
+    <div className="header">
+    {error}
+    </div>
+    </div>
+    );     
+    }
+  }
+  renderInput=(formProps)=>{
+    const className =`field ${formProps.meta.error && formProps.meta.touched ? 'error':''}`;
+    return (
+      <div className={className}>
+    <label>{formProps.label}</label>
+    <input {...formProps.input} />
+   
+    {this.renderError(formProps.meta)}
+    </div>
+    );
+  }
+
+  onSubmit(formValues){
+    console.log(formValues);
+  }
+
+  render(){
+
+  
+  return (
+
+   <form className="ui form error" onSubmit={this.props.handleSubmit(this.onSubmit)}>
+   <Field name="title" component={this.renderInput} label="Enter title"/>
+   <Field name="description" component={this.renderInput} label="Enter description"/>
+   <button type="submit" className="btn btn-primary">Submit</button>
+   </form>
+  )
+  }
+}
+
+const validate=formValues=>{
+ const errors = {}
+ if(!formValues.title){
+   errors.title="Title cannot be blank"
+ }
+ if(!formValues.description){
+  errors.description="Descrition cannot be blank"
+}
+return errors;
+ }
+
+ export default reduxForm({
+  form:'streamCreate',
+  validate
+})(StreamCreate);
